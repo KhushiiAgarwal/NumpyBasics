61c3c1679291a55625db48e07b2411775d15daf0
Khushi A <99123453+KhushiiAgarwal@users.noreply.github.com>
2024-05-02 21:03:40 +0530

    Create Numpyoperation.py

---
+++ Numpyoperation.py
@@ -0,0 +1,25 @@
+
+import pandas as pd
+
+import numpy as np
+
+
+a1=np.ones([4,2], dtype=np.int32)
+
+1=[[10,12,15,18], [16,12,20,12]]
+
+a2=np.array(1)
+
+print(a1.shape)
+
+print(a1.itemsize)
+
+print(a2.shape)
+
+print(a2.itemsize)
+
+print(type(a1))
+
+print(type(a2))
+
+print(a2[-2,1]
